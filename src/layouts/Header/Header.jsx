4f5e3fbbad0cf709fc6d1f0cbd6f1329dbea43ec
Rytkovsky <rytkovsky@outlook.com>
2024-09-09 10:45:17 +0300

    react-gh-pages1

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = ({findEmoji}) => {return <>
 
 <header className={s.header}>
 <Container>
-<h1 className={s.header__title}>Emoji Finder</h1>
+<h1 className={s.header__title}>Emoji Finder!!!!!!</h1>
           <p className={s.header__text}>Find emoji by keywords</p>
           <input onChange={findEmoji}      
             className={s.header__input}
